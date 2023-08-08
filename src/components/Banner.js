57d3c5283566cafe269eb0d7c9f3f3a499510798
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2023-08-08 11:17:26 +0530

    banner add

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,75 @@
 import React from "react";
+import "./Banner.css";
+import {
+  banner1Id,
+  banner2Id,
+  banner3Id,
+  banner4Id,
+  banner5Id,
+} from "../utils/mockdata";
+import { useState } from "react";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  const [y, setY] = useState(0);
+  const prevSlide = () => {
+    if (y <= -4) return;
+    setY(y - 1);
+  };
+  const nextSlide = () => {
+    if (y >= 1) return;
+    setY(y + 1);
+  };
+  return (
+    <div className="banner-container">
+      <div className="heading-container">
+        <h3 className="banner-heading"> Best offers for you</h3>
+        <div className="banner-scroll-btns">
+          <h2
+            className={`${y <= -4 ? `btn-disabled` : ""}`}
+            onClick={prevSlide}
+          >
+            {" "}
+            &larr;{" "}
+          </h2>
+          <h2 className={`${y >= 1 ? `btn-disabled` : ""}`} onClick={nextSlide}>
+            {" "}
+            &rarr;{" "}
+          </h2>
+        </div>
+      </div>
+      <div className="banner-images">
+        <img
+          style={{ transform: `translateX(${425 * y}px)` }}
+          src={banner1Id}
+          alt="banner1"
+        />
+
+        <img
+          style={{ transform: `translateX(${425 * y}px)` }}
+          src={banner2Id}
+          alt="banner2"
+        />
+
+        <img
+          style={{ transform: `translateX(${425 * y}px)` }}
+          src={banner3Id}
+          alt="banner3"
+        />
+
+        <img
+          style={{ transform: `translateX(${425 * y}px)` }}
+          src={banner4Id}
+          alt="banner4"
+        />
+
+        <img
+          style={{ transform: `translateX(${425 * y}px)` }}
+          src={banner5Id}
+          alt="banner5"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/utils/mockdata.js
+++ src/utils/mockdata.js
@@ -21,3 +21,17 @@ export const nightLife =
 
 export const nightLifeClicked =
   "https://b.zmtcdn.com/data/o2_assets/855687dc64a5e06d737dae45b7f6a13b1616149818.png";
+
+// banner images
+const urlOfbanners =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/rng/md/carousel/production/";
+
+export const banner1Id = urlOfbanners + "8e3c441579fe371299ea23001551643f";
+
+export const banner2Id = urlOfbanners + "6ac183e16e51ac257d5dce915b6ada3a";
+
+export const banner3Id = urlOfbanners + "887a5e960bccd776c1c124a8eb2ada0f";
+
+export const banner4Id = urlOfbanners + "2e54588fcfbfae32c57d1d4d3515f6e5";
+
+export const banner5Id = urlOfbanners + "5024fb3f12cc851e695663575b4b09f0";
