5f844da3fe4fb65442c867db56637e15669e438c
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2023-08-09 12:43:32 +0530

    refactor import

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Navbar from "./src/Navbar";
 import Home from "./src/Home";
-import { makeServer } from "./src/utils/server";
 import Tabs from "./src/Tabs";
 import Banner from "./src/components/Banner";
 import { MyRestaurantProvider } from "./src/context/restaurantContext";
@@ -13,10 +12,6 @@ import { UserSelectedCuisines } from "./src/context/selectedCuisineContext";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import RestaurantsMenu from "./src/components/RestaurantsMenu";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// if (process.env.NODE_ENV === "development") {
-//   makeServer({ environment: "development" });
-// }
 
 const AppLayout = () => {
   return (
