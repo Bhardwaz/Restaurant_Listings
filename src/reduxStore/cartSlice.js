cd12a9cd48c2b021668757938f681052685a72ee
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2023-09-05 10:38:05 +0530

    added slice

---
+++ src/reduxStore/cartSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {
+    addToCart: (state, actions) => {
+      state.push(actions.payload);
+    },
+  },
+});
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
